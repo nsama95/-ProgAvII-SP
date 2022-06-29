8a72011041812586f7406ef87deb2e86bf2a216c
nsama95 <36465661+nsama95@users.noreply.github.com>
2022-06-29 17:27:16 -0300

    SegParcial

---
+++ models/People.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const {model, Schema} = mongoose;
+
+
+const starwarsSchema = new Schema({
+        nombre : {type: String, required : true},
+        altura : {type: Number, required : true, min : 0, max : 500},
+        color_pelo : {type: String, required : true},
+        color_ojos :{type: String, required : true},
+        cumpleaños :{type: String, required : true},
+        vive:{type: Boolean, required : true}
+});
+
+
+
+starwarsSchema.set('toJSON', {
+    transform:( (document, starwarsToJSON) => {
+
+        starwarsToJSON.id = starwarsToJSON._id.toString();
+        delete starwarsToJSON._id;
+        delete starwarsToJSON.__v;
+
+    })
+})
+
+module.exports = model('starwarsPeople', starwarsSchema);
---
+++ routes/starwarsRouter.js
@@ -0,0 +1,143 @@
+const starwarsRouter = require('express').Router();
+const People = require('../models/People');
+const { verifyToken } = require('../utils/middelwares');
+
+
+
+//TOKEN 
+
+starwarsRouter.use(verifyToken);
+
+//traer todos
+starwarsRouter.get("/", (req, res, next) => {
+
+    People.find({}).then(personajes => {
+            res.json(personajes);
+    })
+    
+    .catch (error => {
+
+        next(error);
+    })
+
+});
+
+//traer por id
+starwarsRouter.get("/:id", (req, res, next) => {
+
+    const id = req.params.id;
+
+    People.findById(id).then(personaje => {
+
+        if (personaje){
+            res.json(personaje);
+        }
+
+        res.status(404).end();
+
+    })
+
+    .catch( error => {
+
+        next(error);
+    })
+
+});
+
+//borrar personaje
+starwarsRouter.delete("/:id", (req, res, next) => {
+
+    const id = req.params.id;
+
+    People.findByIdAndRemove(id)
+    
+    .then(result => {
+
+        if (result){
+            res.status(204).end();
+        }
+        res.status(404).end();
+    })
+    
+    .catch( error => {
+
+        next(error);
+    })
+
+});
+
+//alta de personaje 
+starwarsRouter.post("/", (req, res, next) => {
+
+    const {nombre, altura, color_pelo, color_ojos, cumpleaños,vive} = req.body;
+
+        const nuevoPersonaje = new People({
+            nombre, altura, color_pelo, color_ojos, cumpleaños,vive
+        });
+
+        nuevoPersonaje.save()
+
+        .then(People => res.json(People))
+        
+        .catch( (error) => {
+            next (error);
+        })
+
+});
+
+
+
+//modificar personaje
+starwarsRouter.put("/:id", (req, res, next) => {
+
+    const id = req.params.id;
+
+    const {nombre, altura, color_pelo, color_ojos, cumpleaños,vive} = req.body;
+
+    const info = {nombre, altura, color_pelo, color_ojos, cumpleaños,vive}; 
+    const infoUpdate ={};
+
+    if (nombre)
+    {
+        infoUpdate.nombre = nombre;
+    }
+
+    if (altura)
+    {
+        infoUpdate.altura = altura;
+    }
+
+    if (color_pelo)
+    {
+        infoUpdate.color_pelo = color_pelo;
+    }
+
+    if (color_ojos)
+    {
+        infoUpdate.color_ojos = color_ojos;
+    }
+
+    if (cumpleaños)
+    {
+        infoUpdate.cumpleaños = cumpleaños;
+    }
+    if (vive)
+    {
+        infoUpdate.vive = vive;
+    }
+    
+    People.findByIdAndUpdate(id, info, {new:true}).then(People => {
+
+       if(People)
+        res.json(People);
+        res.status(400).end();
+    })
+
+    .catch( error => {
+
+        next (error);
+    }) 
+
+});
+
+module.exports = starwarsRouter;
